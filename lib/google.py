bc8b3c36d416442e1f434367ade0d9ab2272b740
Tomáš Strachoň <74866545+GameStracho@users.noreply.github.com>
2026-06-15 09:07:21 +0200

    Version 1.0
    Add StrachyBot v1.0 from February 2023

---
+++ lib/google.py
@@ -0,0 +1,123 @@
+import io
+import json
+import os
+import datetime
+from collections import namedtuple
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.http import MediaFileUpload, MediaIoBaseDownload
+from googleapiclient.discovery import build
+from google.oauth2.credentials import Credentials
+from google.auth.transport.requests import Request
+
+from lib import console
+
+def create_service(client_secret_file, api_name, api_version, *scopes, prefix=''):
+    CLIENT_SECRET_FILE = client_secret_file
+    API_SERVICE_NAME = api_name
+    API_VERSION = api_version
+    SCOPES = [scope for scope in scopes[0]]
+    
+    credentials = None
+    working_dir = os.getcwd()
+    token_dir = "./src/"
+    token_file = f"token_{API_SERVICE_NAME}_{API_VERSION}{prefix}.json"
+
+    ### Check if token dir exists first, if not, create the folder
+    if not os.path.exists(os.path.join(working_dir, token_dir)):
+        os.mkdir(os.path.join(working_dir, token_dir))
+
+    if os.path.exists(os.path.join(working_dir, token_dir, token_file)):
+        credentials = Credentials.from_authorized_user_file(os.path.join(working_dir, token_dir, token_file), SCOPES)
+        # with open(os.path.join(working_dir, token_dir, token_file), 'rb') as token:
+        #   cred = pickle.load(token)
+
+    if not credentials or not credentials.valid:
+        if credentials and credentials.expired and credentials.refresh_token:
+            credentials.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRET_FILE, SCOPES)
+            credentials = flow.run_local_server(port=0)
+
+        with open(os.path.join(working_dir, token_dir, token_file), "w") as token:
+            token.write(credentials.to_json())
+
+    try:
+        service = build(API_SERVICE_NAME, API_VERSION, credentials=credentials, static_discovery=False)
+        console.log("GOOGLE", (
+            API_SERVICE_NAME +
+            f" {API_VERSION} " +
+            "service created successfully."
+        ))
+        return service
+    except Exception as e:
+        print(e)
+        console.log("GOOGLE", (
+            f"Failed to create service instance for {API_SERVICE_NAME}"
+        ))
+        os.remove(os.path.join(working_dir, token_dir, token_file))
+        return None
+
+
+def convert_to_RFC_datetime(year=1900, month=1, day=1, hour=0, minute=0):
+    dt = datetime.datetime(year, month, day, hour, minute, 0).isoformat() + "Z"
+    return dt
+
+CLIENT_SECRET_FILE = "./src/credentials.json"
+API_NAME = "drive"
+API_VERSION = "v3"
+SCOPES = ["https://www.googleapis.com/auth/drive"]
+
+service = create_service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
+
+def upload_file(file_name: str):
+    file_metadata = {
+        "name": file_name,
+        "parents": ["1vE4zPJWbXdBlqDoUPJ40hLSVkRNW0ILA"] # folder ids
+    }
+
+    media_content = MediaFileUpload(f"./src/{file_name}", mimetype="application/json")
+
+    file = service.files().create(
+        body=file_metadata,
+        media_body=media_content
+    ).execute()
+
+    print(file)
+
+    console.log("GOOGLE", f"File {file_name} successfully uploaded.")
+
+
+def replace_file():
+    file_id: str = "18A54d4_d5Nsil-E3uRJV1sYD_B3bVNwz"
+
+    media_content = MediaFileUpload("./src/user_data.json", mimetype="application/json")
+
+    service.files().update(
+        fileId=file_id,
+        media_body=media_content
+    ).execute()
+
+    console.log("GOOGLE", f"File user_data.json successfully replaced.")
+
+
+def download_file():
+    file_id = "18A54d4_d5Nsil-E3uRJV1sYD_B3bVNwz"
+    file_name = "user_data.json"
+    
+    request = service.files().get_media(fileId=file_id)
+    
+    fh = io.BytesIO()
+    downloader = MediaIoBaseDownload(fd=fh, request=request)
+
+    done: bool = False
+
+    while not done:
+        status, done = downloader.next_chunk()
+    
+    console.log("GOOGLE", f"File {file_name} successfully downloaded.")
+    
+    fh.seek(0)
+    
+    with open(f"./src/{file_name}", "wb") as f:
+        f.write(fh.read())
+        f.close()
---
+++ lib/tic.py
@@ -0,0 +1,218 @@
+from typing import Dict, List
+from math import sqrt
+
+import discord
+from discord import Color
+
+from lib import console, stats
+
+
+class TicTacToeButtons(discord.ui.View):
+    def __init__(self, game_id: int, grid_size: int) -> None:
+        super().__init__(timeout=None)
+        self.game_id = game_id
+
+        
+        for i in range(grid_size):
+            for j in range(grid_size):
+                button: discord.ui.Button = discord.ui.Button(
+                    custom_id=str((i * grid_size) + j), label="\u1CBC", row=i)
+                button.callback = self.button_callback
+                self.add_item(button)
+
+    async def button_callback(self, interaction: discord.Interaction):
+        await interaction.response.send_message(
+               ephemeral=True, content="responding...", delete_after=0)
+        await button_pressed(
+            self.game_id, int(interaction.data["custom_id"]),
+            interaction.user.id)
+
+
+games: Dict[int, dict] = {}
+
+
+async def start(
+        interaction: discord.Interaction, opponent: discord.User, grid_size: int) -> None:
+    user: discord.User = interaction.user
+    msg: str = f"🟣 - {user.mention}\n🟠 - {opponent.mention}"
+    embed = discord.Embed(
+        color=Color.purple(), title="Tic-Tac-Toe", description=msg)
+
+    icon: discord.File = discord.File(
+        "./src/images/icon_tic.png", filename="icon_tic.png")
+    embed.set_thumbnail(url="attachment://icon_tic.png")
+    embed.description += f"\n\nIt's {user.mention}'s turn."
+
+    view: discord.ui.View = TicTacToeButtons(user.id, grid_size)
+
+    game_info: dict = {
+        "interaction": interaction,
+        "users": [user, opponent],
+        "embed": embed,
+        "view": view,
+        "turn": 0,
+        "msg": msg
+    }
+    
+    if user.id in games:
+        await __remove_buttons(user.id)
+
+    games[user.id] = game_info
+
+    console.log("TIC", (
+        f"{interaction.user.display_name} has started a new game "
+        f"against {opponent.display_name}."))
+
+    stats.edit(user, "tic", "games", 1)
+    stats.edit(opponent, "tic", "games", 1)
+    await interaction.response.send_message(
+        file=icon, embed=embed, view=view)
+
+
+async def button_pressed(game_id: int, button_num: int, user_id: int) -> None:
+    game_info: dict = games[game_id]
+    embed: discord.Embed = game_info["embed"]
+    
+    if game_info["users"][game_info["turn"]].id == user_id:
+        if change_button_label(game_id, button_num):
+            if __check_end(game_id):
+                await __remove_buttons(game_id)
+                games.pop(game_id)
+            else:
+                __switch_turns(game_id)
+                interaction: discord.Interaction = game_info["interaction"]
+                view: discord.ui.View = game_info["view"]
+                await interaction.edit_original_response(embed=embed, view=view)
+
+
+def change_button_label(game_id: int, button_num: int) -> bool:
+    game_info: dict = games[game_id]
+    view: discord.ui.View = game_info["view"]
+
+    if view.children[button_num].label == "\u1CBC":
+        label: str = ""
+        if game_info["turn"]:
+            label = "🟠"
+        else:
+            label = "🟣"
+        view.children[button_num].label = label
+        return True
+    else:
+        return False
+
+
+def __check_end(game_id: int) -> bool:
+    game_info: dict = games[game_id]
+    view: discord.ui.View = game_info["view"]
+    embed: discord.Embed = game_info["embed"]
+
+    symbol: str = ""
+    if game_info["turn"]:
+            symbol = "🟠"
+    else:
+        symbol = "🟣"
+    
+    if (__check_rows(view, symbol) or
+        __check_columns(view, symbol) or 
+        __check_diagonals(view, symbol)):
+        user = game_info['users'][game_info['turn']]
+        embed.description = f"{game_info['msg']}\n\n{user.mention} won! *+15 xp for winner*"
+        stats.edit(user, "tic", "wins", 1)
+        stats.edit(user, "", "exp", 15)
+        return True
+
+    for button in view.children:
+        if button.label == "\u1CBC":
+            return False
+    embed.description = f"{game_info['msg']}\n\nGame ended in a draw. *+5 xp for both*"
+    stats.edit(game_info['users'][0], "", "exp", 5)
+    stats.edit(game_info['users'][1], "", "exp", 5)
+    embed.color = Color.lighter_grey()
+    return True
+
+
+def __check_rows(view: discord.ui.View, symbol: str) -> bool:
+    rows_num: int = int(sqrt(len(view.children)))
+    for i in range(rows_num):
+        for j in range(rows_num - 2):
+            #print(f"rows: {(i * rows_num) + j}, {(i * rows_num) + j + 1}, {(i * rows_num) + j + 2}")
+            if (view.children[(i * rows_num) + j].label == symbol and
+                view.children[(i * rows_num) + j + 1].label == symbol and
+                view.children[(i * rows_num) + j + 2].label == symbol):
+                    return True
+    return False
+
+
+def __check_columns(view: discord.ui.View, symbol: str) -> bool:
+    columns_num: int = int(sqrt(len(view.children)))
+    for i in range(columns_num):
+        for j in range(columns_num - 2):
+            #print(f"columns: {i + (j * columns_num)}, {i + ((j + 1) * columns_num)}, {i + ((j + 2) * columns_num)}")
+            if (view.children[i + (j * columns_num)].label == symbol and
+                view.children[i + ((j + 1) * columns_num)].label == symbol and
+                view.children[i + ((j + 2) * columns_num)].label == symbol):
+                    return True
+    return False
+
+
+def __check_diagonals(view: discord.ui.View, symbol: str) -> bool:
+    diagonals_num: int = int(sqrt(len(view.children)))
+
+    starting_points: List[List[int]] = [[], []]
+
+    for i in range(diagonals_num - 2):
+        for j in range(diagonals_num - 2):
+            starting_points[0].append((i*diagonals_num) + j + 2)
+            starting_points[1].append((i*diagonals_num) + j)
+    
+    for i in range(2):
+        step: int = (diagonals_num - 1) + (i * 2)
+        #print(f"step: {step}")
+        for starting_point in starting_points[i]:
+            #print(f"diagonals: {starting_point}, {starting_point + step}, {starting_point + 2 * step}")
+            if (view.children[starting_point].label == symbol and
+                view.children[starting_point + step].label == symbol and
+                view.children[starting_point + 2 * step].label == symbol):
+                    return True
+
+
+def __switch_turns(game_id: int) -> None:
+    game_info: dict = games[game_id]
+    embed: discord.Embed = game_info["embed"]
+    if game_info["turn"]:
+        game_info["turn"] = 0
+        embed.color = Color.purple()
+    else:
+        game_info["turn"] = 1
+        embed.color = Color.orange()
+    embed.description = (
+        f"{game_info['msg']}"
+        f"\n\nIt's {game_info['users'][game_info['turn']].mention}'s turn.")
+
+
+async def __remove_buttons(game_id: int) -> None:
+    game_info: dict = games[game_id]
+    view: discord.ui.View = game_info["view"]
+    embed: discord.Embed = game_info["embed"]
+
+    embed.description += "\n\n"
+    
+    rows_num: int = int(sqrt(len(view.children)))
+    label: str = ""
+    for i in range(rows_num):
+        for j in range(rows_num):
+            label = view.children[(i * rows_num) + j].label
+            if label == "\u1CBC":
+                embed.description += " ` ⚫ `"
+            elif label == "🟣":
+                embed.description += " ` 🟣 `"
+            else:
+                embed.description += " ` 🟠 `"
+            
+            if j == rows_num - 1:
+                embed.description += "\n"
+    try:
+        interaction: discord.Interaction = game_info["interaction"]
+        await interaction.edit_original_response(embed=embed, view=None)
+    except:
+        pass
