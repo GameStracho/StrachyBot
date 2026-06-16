5fa8dfddbd1ebb2cc98971a91861f4b9d8e3231e
Tomáš Strachoň <74866545+GameStracho@users.noreply.github.com>
2026-06-16 19:30:51 +0200

    feat: add docker support

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from discord.ext import commands
 from discord import app_commands
 from dotenv import load_dotenv
 
-from lib import console, wordle, tic, quote, google
+from lib import console, wordle, tic, quote
 
 load_dotenv()
 TOKEN = getenv("DISCORD_TOKEN")
