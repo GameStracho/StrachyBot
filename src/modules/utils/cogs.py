086cfca4ee23d1b1ce111555029a6c21d50dd7c9
Tomáš Strachoň <74866545+GameStracho@users.noreply.github.com>
2026-07-12 14:23:54 +0200

    refactor: move utils command logic to cogs.py

--- src/modules/utils/cogs.py
+++ src/modules/utils/cogs.py
@@ -1,7 +1,8 @@
 from typing import Optional
+import datetime
 
-from modules.utils import logic
 from shared.bot import StrachyBot
+from shared import console, messages
 
 import discord
 from discord.ext import commands
@@ -16,7 +17,33 @@ class UtilsCog(commands.Cog):
     async def info(
         self,
         interaction: discord.Interaction) -> None:
-        await logic.show_info(interaction, self.bot.start_time)
+        console.log_info(f"/info: User {interaction.user.display_name} used the command.")
+
+        try:
+            uptime: datetime.timedelta = discord.utils.utcnow() - self.bot.start_time
+
+            embed: discord.Embed = discord.Embed(
+                color=discord.Color.blue(),
+                title="Bot information",
+                description="Discord bot with fun mini-games like *Wordle* and *Tic-Tac-Toe*."
+            )
+            
+            embed.add_field(name="Ping", value=f"{round(interaction.client.latency * 1000)} ms", inline=True)
+            embed.add_field(name="Uptime", value=f"{str(uptime).split('.')[0]}", inline=True)
+
+            embed.add_field(name="Version", value="v1.0.4 (2026-07-03)", inline=True)
+            embed.add_field(name="Changelog", value=(
+                "- Added `/info`, `/announcement` and `error` icons"
+                "\n- Scaled down `wordle` and `tic-tac-toe` icons"
+            ), inline=False)
+
+            icon: discord.File = discord.File(
+            "./src/modules/utils/info.png", filename="info.png")
+            embed.set_thumbnail(url="attachment://info.png")
+            
+            await interaction.response.send_message(embed=embed, file=icon)
+        except Exception as e:
+            await messages.handle_error(e, interaction)
 
 
     @app_commands.command(name="announcement", description="Make announcements in chat.")
@@ -25,4 +52,19 @@ class UtilsCog(commands.Cog):
         interaction: discord.Interaction,
         title: Optional[str] = "",
         message:  Optional[str] = "") -> None:
-        await logic.announce(interaction, title, message)
+        console.log_info(f"/info: User {interaction.user.display_name} passed title '{title}' and '{message}'.")
+    
+        try:
+            embed: discord.Embed = discord.Embed(color=discord.Color.yellow())
+            if title:
+                embed.title = title
+            if message:
+                embed.description = message
+
+            icon: discord.File = discord.File(
+            "./src/modules/utils/announcement.png", filename="announcement.png")
+            embed.set_thumbnail(url="attachment://announcement.png")
+            
+            await interaction.response.send_message(embed=embed, file=icon)
+        except Exception as e:
+            await messages.handle_error(e, interaction)
