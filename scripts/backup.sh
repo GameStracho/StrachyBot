4aed0e7426cc7d7f8cbf97376470fff53229f962
Tomáš Strachoň <74866545+GameStracho@users.noreply.github.com>
2026-07-12 13:18:56 +0200

    fix: filter changes before commit

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -39,14 +39,35 @@ if [ ! "$(docker ps -q -f name=^${CONTAINER_NAME}$)" ]; then
     exit 1
 fi
 
+BACKUP_EXISTS=
+
+if [[ -f "${FULL_BACKUP_PATH}" ]]; then
+    BACKUP_EXISTS=true
+fi
+
 echo "Extracting database contents from the '${CONTAINER_NAME}' Docker container..."
 docker exec -t "$CONTAINER_NAME" pg_dump -U "$DB_USER" -d "$DB_NAME" > "$FULL_BACKUP_PATH"
 echo "SQL dump successfully written to: ${FULL_BACKUP_PATH}."
 
+echo "Tracking changes to '${BACKUP_FILE_NAME}'..."
 cd "$BACKUP_REPO_DIR"
+git add "$BACKUP_FILE_NAME"
+
+if [[ ! -z "${BACKUP_EXISTS}" ]]; then
+    echo "Filtering database changes..."
+    # Filter out git headers (+++ or ---) and '\restrict' and '\unrestrict' lines
+    BACKUP_CHANGED=$(git diff --cached | grep -E '^[+-][^+-]' | grep -vE '(restrict|unrestrict)' || true)
+
+    # Revert backup if no changes occurred and exit
+    if [[ -z "${BACKUP_CHANGED}" ]]; then
+        echo "No meaningful database changes detected. Skipping commit."
+        git restore --staged "$BACKUP_FILE_NAME"
+        git restore "$BACKUP_FILE_NAME"
+        exit 0
+    fi
+fi
 
 echo "Committing the changes..."
-git add "$BACKUP_FILE_NAME"
 git -c user.name="$GIT_USER" -c user.email="$GIT_EMAIL" commit -m "[${CURRENT_DATE}] automatic backup."
 
 echo "Pushing changes to the remote repository..."
