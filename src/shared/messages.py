23e682f6b657817266b9be853bceb1a46b36d449
Tomáš Strachoň <74866545+GameStracho@users.noreply.github.com>
2026-07-15 19:54:16 +0200

    feat: improve error handling

--- src/shared/messages.py
+++ src/shared/messages.py
@@ -1,10 +1,16 @@
-from shared import console
+import traceback
 
 import discord
 from discord import Color
 
-async def handle_error(ex: Exception, interaction: discord.Interaction):
-    console.log_error(f"An unexpected error occurred for user {interaction.user.display_name}: \n{ex}")
+from shared import console
+
+async def handle_error(command: str, interaction: discord.Interaction):
+    """
+    Print error message with details to console and send generic message to user.
+    IMPORTANT: only call from an except block!
+    """
+    console.log_error(f"{command}: An unexpected error occurred for user {interaction.user.display_name}: \n{traceback.format_exc()}")
 
     embed: discord.Embed = discord.Embed(color=Color.red())
     embed.title = "Error"
