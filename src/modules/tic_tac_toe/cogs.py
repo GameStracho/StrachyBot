39569089dc1b82f3477e01260a838936a7e11b63
Tomáš Strachoň <74866545+GameStracho@users.noreply.github.com>
2026-07-28 20:44:02 +0200

    feat: remove old tic_tac_toe command with its logic

--- src/modules/tic_tac_toe/cogs.py
+++ src/modules/tic_tac_toe/cogs.py
@@ -3,7 +3,6 @@ from discord.ext import commands
 from discord import app_commands
 
 from shared import console, bot, messages, helpers
-from modules.tic_tac_toe import logic
 from .ui import TicTacToeView, get_player_emojis, PLAYER_COLOR
 from .game import TicTacToeGame
 
@@ -11,24 +10,6 @@ class TicCog(commands.Cog):
     def __init__(self, bot: bot.StrachyBot) -> None:
         self.bot = bot
 
-    @app_commands.command(
-        name="tic_tac_toe", description="Challenge someone in Tic-Tac-Toe")
-    @app_commands.choices(grid_size=[
-        discord.app_commands.Choice(name="3x3", value=3),
-        discord.app_commands.Choice(name="4x4", value=4),
-        discord.app_commands.Choice(name="5x5", value=5)
-    ])
-    async def tic_play(
-        self, interaction: discord.Interaction, opponent: discord.User,
-        grid_size: app_commands.Choice[int]) -> None:
-        user_id: int = interaction.user.id
-        mention: str = self.bot.user.mention if self.bot.user is not None else ""
-        if user_id == opponent.id:
-            await interaction.response.send_message(
-                ephemeral=True,
-                content=f"To play singleplayer choose {mention} as your opponent. - Coming soon")
-        else:
-            await logic.start(interaction, opponent, grid_size.value)
 
     @app_commands.command(
         name="tic-tac-toe", description="Challenge someone in a 1v1 Tic-Tac-Toe match")
