fd60b727adc579b8c1b45090c52decc92e8b91fd
Tomáš Strachoň <74866545+GameStracho@users.noreply.github.com>
2026-08-07 16:12:28 +0200

    feat: remove old commands and logic

--- src/modules/wordle/cogs.py
+++ src/modules/wordle/cogs.py
@@ -2,7 +2,6 @@ import discord
 from discord import app_commands
 from discord.ext import commands
 
-from modules.wordle import logic
 from shared import bot, console, messages
 
 from .game import WordleGame
@@ -12,19 +11,6 @@ from .ui import WordleView
 class WordleCog(commands.Cog):
     def __init__(self, bot: bot.StrachyBot) -> None:
         self.bot = bot
-
-    @app_commands.command(
-        name="wordle_play", description="Try to guess a word in 6 tries.")
-    async def wordle_play(self, interaction: discord.Interaction) -> None:
-        await logic.start(interaction)
-
-    @app_commands.command(name="wordle_guess", description="Guess a word in Wordle.")
-    async def wordle_guess(self, interaction: discord.Interaction, word: str) -> None:
-        if logic.is_playing(interaction.user.id):
-            await logic.guess(word, interaction)
-        else:
-            await interaction.response.send_message(
-                ephemeral=True, content="You have to start a new game first.")
 
     @app_commands.command(name="wordle", description="Try to guess a 5-letter word in 6 tries.")
     async def wordle(self, interaction: discord.Interaction) -> None:
