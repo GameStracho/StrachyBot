a09d572564891231046523c3cb20572e225ca9fd
Tomáš Strachoň <74866545+GameStracho@users.noreply.github.com>
2026-08-16 12:07:12 +0200

    fix: enable seasonal emojis and colors

--- src/shared/ui/player.py
+++ src/shared/ui/player.py
@@ -52,8 +52,6 @@ def get_player_colors(date: datetime | None = None) -> tuple[discord.Color, disc
     """
     Returns player's and opponent's colors based on selected date.
     """
-    return (discord.Color.purple(), discord.Color.orange())
-
     if not date:
         date = datetime.now(UTC)
 
@@ -97,8 +95,6 @@ def get_player_emojis(date: datetime | None = None) -> tuple[str, str]:
     """
     Returns player's and opponent's emojis based on selected date.
     """
-    return ("🟣", "🟠")
-
     if not date:
         date = datetime.now(UTC)
 
